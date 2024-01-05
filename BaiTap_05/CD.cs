0a4a5ec19af97a763de0c3d9cb40849f855f97e8
ADMIN <ADMIN@PC335>
2024-01-05 11:20:29 +0700

    Add project files.

---
+++ BaiTap_05/CD.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaiTap_05
+{
+    class CD
+    {
+        private int macd;
+        private string tuacd;
+        private string casi;
+        private int sobh;
+        private int dongia;        
+        public CD()
+        {
+        }
+        public CD(int macd)
+        {
+            this.macd = macd;
+        }
+        public CD(int macd, string tuacd, string casi, int sobh, int dongia)
+        {
+            this.macd = macd;
+            this.tuacd = tuacd;
+            this.casi = casi;
+            this.sobh = sobh;
+            this.dongia = dongia;
+        }
+        public int MaCD
+        {
+            set { this.macd = value; }
+            get { return macd; }
+        }
+        public string TuaCD
+        {
+            set { this.tuacd = value; }
+            get { return tuacd; }
+        }
+        public string CaSi
+        {
+            set { this.casi = value; }
+            get { return casi; }
+        }
+        public int SoBH
+        {
+            set { this.sobh = value; }
+            get { return sobh; }
+        }
+        public int DonGia
+        {
+            set { this.dongia = value; }
+            get { return dongia; }
+        }
+        public string Tostring()           
+        {
+            return string.Format("{0,10}", "{1,30}", "{2,30}", "{3,10}", "{4,15:#,##0}",
+                macd, tuacd, casi, sobh, dongia);
+        }
+
+    }
+}
---
+++ BaiTap_05/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaiTap_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             menu();
+        }
+        static void menu()
+        {
+            QuanLyCD quanLyCD = new QuanLyCD();
+            int chon = 0;
+            do
+            {
+                Console.WriteLine("******Chuong trinh quan ly CD******");
+                Console.WriteLine("1. Them CD:");
+                Console.WriteLine("2. Tinh gia trung binh:");
+                Console.WriteLine("3. Sap xep CD giam dan theo gia thanh:");
+                Console.WriteLine("4. Sap xep CD tang dan theo tua CD:");
+                Console.WriteLine("5. Xuat toan bo CD:");
+                Console.WriteLine("0. Thoat:");
+                Console.WriteLine("-------------------------------------");
+                Console.WriteLine("Ban da chon:");
+                chon = int.Parse(Console.ReadLine());
+                switch (chon)
+                {
+                    case 1:
+                        CD cd = new CD();
+                        Console.Write("Nhap ma CD");
+                        cd.MaCD = int.Parse(Console.ReadLine());
+                        Console.Write("Nhap tua CD");
+                        cd.TuaCD = Console.ReadLine();
+                        Console.Write("Nhap ca sy");
+                        cd.CaSi = Console.ReadLine();
+                        Console.Write("Nhap so bai hat");
+                        cd.SoBH = int.Parse(Console.ReadLine());
+                        Console.Write("Nhap gia thanh");
+                        cd.DonGia = int.Parse(Console.ReadLine());
+                        quanLyCD.themCD(cd);
+                        break;
+                    case 2:
+                        double kq = quanLyCD.tinhGiaTB();
+                        Console.WriteLine("Gia thanh trung binh: {0:#,##0.00}", kq);
+                        break;
+                    case 3:
+                        quanLyCD.SapXepGiamTheoGiaThanh();
+                        Console.WriteLine("Da sap xep theo gia thanh giam dan");
+                        break;
+                    case 4:
+                        quanLyCD.SapXepGiamTheoTuaCD();
+                        Console.WriteLine("Da sap xep theo tua CD tang dan!");
+                        break;
+                    case 5:
+                        quanLyCD.Xuat();
+                        break;
+                    case 0:
+                        Console.WriteLine("Tam biet. Hen gap lai");
+                        Console.ReadLine();
+                        break;
+                }
+            } while (chon != 0);
+        }
+    }
+}
